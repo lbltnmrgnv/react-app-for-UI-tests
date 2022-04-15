1464d48694bf11404c7c53a96fe1d58a360a4047
Mikhail Pankov <lbltnmrgnv@ya.ru>
2022-04-15 17:03:30 +0700

    update dependencies and add some fixes to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import LandingPage from './components/pages/LandingPage'
 import LoginPage from './components/pages/LoginPage'
@@ -13,13 +13,13 @@ export default function App() {
     return (
         <Router>
             <div>
-                <Switch>
-                    <Route exact path="/" component={ LandingPage } />
-                    <Route path="/login" component={ LoginPage } />
-                    <Route path="/register" component={ RegisterPage } />
-                    <Route path="/forget-password" component={ ForgetPasswordPage } />
-                    <Route path="/home" component={ HomePage } />
-                </Switch>
+                <Routes>
+                    <Route exact path="/" element={<LandingPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                    <Route path="/forget-password" element={<ForgetPasswordPage />} />
+                    <Route path="/home" element={<HomePage />} />
+                </Routes>
             </div>
         </Router>
     )
